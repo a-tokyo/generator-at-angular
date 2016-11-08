92f6bc534bea094d9a64d49326b7fa5d8da24665
Tokyo <ahmed.tokyo1@gmail.com>
2016-11-08 17:18:50 +0200

    component generates file names correctly

--- generators/component/src/files.js
+++ generators/component/src/files.js
@@ -6,9 +6,24 @@ module.exports = function (AngularATGenerator) {
 
     AngularATGenerator.prototype.copyCopmFiles = function copyFiles() {
       this.fs.copyTpl(
-          this.templatePath('**/*'),
-          this.destinationPath('src/app/components/'+this.props.componentName),
-          this
-      );
+      this.templatePath('component.html'),
+      this.destinationPath('src/app/components/'+this.props.componentName+'/'+this.props.componentName+'.html'),
+      this
+    );
+      this.fs.copyTpl(
+      this.templatePath('component.scss'),
+      this.destinationPath('src/app/components/'+this.props.componentName+'/'+this.props.componentName+'.scss'),
+      this
+    );
+      this.fs.copyTpl(
+      this.templatePath('component.module.js'),
+      this.destinationPath('src/app/components/'+this.props.componentName+'/'+this.props.componentName+'.module'+'.js'),
+      this
+    );
+      this.fs.copyTpl(
+      this.templatePath('component.directive.js'),
+      this.destinationPath('src/app/components/'+this.props.componentName+'/'+this.props.componentName+'.directive'+'.js'),
+      this
+    );
     };
 };
