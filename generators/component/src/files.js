014ef73e162a1f65d598376e084553b45e66dd27
Tokyo <ahmed.tokyo1@gmail.com>
2016-11-08 04:13:02 +0200

    working on comp sub generator

---
+++ generators/component/src/files.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var mkdirp = require('mkdirp');
+
+module.exports = function (AngularATGenerator) {
+
+    AngularATGenerator.prototype.copyCopmFiles = function copyFiles() {
+
+        this.fs.copy(
+            this.templatePath('footer.html'),
+            this.destinationPath('src/app/components')
+        );
+    };
+
+};
