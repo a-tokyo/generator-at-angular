a73b3823b11c0d3807f52bc2754a56a01c1f1495
Tokyo <ahmed.tokyo1@gmail.com>
2016-11-10 00:30:39 +0200

    added a checking condition in directive generator to know whether it is component nested and act accordingly

--- generators/directive/src/files.js
+++ generators/directive/src/files.js
@@ -6,26 +6,41 @@ var utils = require("../../../utils.js");
 module.exports = function (AngularATGenerator) {
 
     AngularATGenerator.prototype.copyCopmFiles = function copyFiles() {
-      var data = {'directiveName': this.props.directiveName, 'directiveNameCamel': _.camelCase(this.props.directiveName), 'needsPartial': this.props.needsPartial};
-      if(this.props.needsPartial){
-      this.fs.copyTpl(
-      this.templatePath('directive.html'),
-      this.destinationPath(this.destinationRoot()+'/src/app/core/directives/'+data.directiveName+'/'+data.directiveName+'.html'),
-      data
-    );
-      this.fs.copyTpl(
-      this.templatePath('directive.scss'),
-      this.destinationPath(this.destinationRoot()+'/src/app/core/directives/'+data.directiveName+'/'+data.directiveName+'.scss'),
-      data
-    );
-    }
-      this.fs.copyTpl(
-      this.templatePath('directive.directive.js'),
-      this.destinationPath(this.destinationRoot()+'/src/app/core/directives/'+data.directiveName+'/'+data.directiveName+'.directive'+'.js'),
-      data
-    );
+      var itemRelPath = this.props.directiveName;
+      var relPathAsArray = itemRelPath.split('/');
+      var directiveName = relPathAsArray[relPathAsArray.length-1];
 
-    var coreModulesWriteLine = "require('./directives/"+data.directiveName+"/"+data.directiveName+".directive')(shared);";
-    utils.addToFile("core.module.js",coreModulesWriteLine,utils.DIRECTIVE_MARKER,this.destinationRoot()+"/src/app/core");
+      var data = {'directiveName': directiveName, 'directiveNameCamel': _.camelCase(directiveName), 'needsPartial': this.props.needsPartial};
+
+      if(relPathAsArray.length===1) {
+        var appRelPath = '/src/app/core/directives';
+
+        if(this.props.needsPartial){
+        this.fs.copyTpl(
+        this.templatePath('directive.html'),
+        this.destinationPath(this.destinationRoot()+appRelPath+'/'+data.directiveName+'/'+data.directiveName+'.html'),
+        data
+      );
+        this.fs.copyTpl(
+        this.templatePath('directive.scss'),
+        this.destinationPath(this.destinationRoot()+appRelPath+'/'+data.directiveName+'/'+data.directiveName+'.scss'),
+        data
+      );
+      }
+        this.fs.copyTpl(
+        this.templatePath('directive.directive.js'),
+        this.destinationPath(this.destinationRoot()+appRelPath+'/'+data.directiveName+'/'+data.directiveName+'.directive'+'.js'),
+        data
+      );
+
+      var coreModulesWriteLine = "require('./directives/"+data.directiveName+"/"+data.directiveName+".directive')(shared);";
+      utils.addToFile("core.module.js",coreModulesWriteLine,utils.DIRECTIVE_MARKER,this.destinationRoot()+"/src/app/core");
+
+    }else{
+      var parentComponentName = pathAsArray[relPathAsArray.length-2];
+    }
+
+
+
     };
 };
