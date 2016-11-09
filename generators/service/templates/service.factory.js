d7a144f7ef4bb45043f9837a5de2df176665b0b4
Tokyo <ahmed.tokyo1@gmail.com>
2016-11-09 02:52:56 +0200

    service template bugFix

--- generators/service/templates/service.factory.js
+++ generators/service/templates/service.factory.js
@@ -4,7 +4,7 @@ export default function (app) {
     app
         .factory('<%= serviceNameCamel %>', <%= serviceNameCamel %>Factory);
 
-        function storeFactory () {
+        function s<%= serviceNameCamel %>Factory () {
             return {
 
             };
