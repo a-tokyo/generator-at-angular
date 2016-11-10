f1afd2b3a46799fdfe6731ce24c9ee336d23c92d
Tokyo <ahmed.tokyo1@gmail.com>
2016-11-10 03:04:57 +0200

    generator directive in component prototype

--- generators/directive/src/files.js
+++ generators/directive/src/files.js
@@ -19,6 +19,7 @@ module.exports = function (AngularATGenerator) {
         this.destinationPath(fullPath+'/'+data.directiveName+'.directive'+'.js'),
         data
         );
+        // add the directive to the core.modules file of shared directives
         var coreModulesWriteLine = "require('./directives/"+data.directiveName+"/"+data.directiveName+".directive')(shared);";
         utils.addToFile("core.module.js",coreModulesWriteLine,utils.DIRECTIVE_MARKER,this.destinationRoot()+"/src/app/core");
       }else{
@@ -32,6 +33,12 @@ module.exports = function (AngularATGenerator) {
         this.destinationPath(fullPath+'/'+data.directiveName+'.directive'+'.js'),
         data
         );
+        // import directive into controller
+        var importInParentModuleWriteLine = "import * as "+ data.directiveNameCamel+ "Directive" +" from './directives/"+  data.directiveName +'/'+ data.directiveName + ".directive';";
+        utils.addToFile(parentName+".module.js",importInParentModuleWriteLine,utils.IMPORT_DIRECTIVE_MARKER,this.destinationRoot()+appRelPath+'/'+parentPath);
+        //add directive to parent module
+        var addDirToParentModuleWriteLine = "componentModule.directive('"+ data.directiveNameCamel +"', " +data.directiveNameCamel+ "Directive" +");";
+        utils.addToFile(parentName+".module.js",addDirToParentModuleWriteLine,utils.ADD_DIRECTIVE_TOMODULE_MARKER,this.destinationRoot()+appRelPath+'/'+parentPath);
       }
       //Write view templates if needed
       if(this.props.needsPartial){
