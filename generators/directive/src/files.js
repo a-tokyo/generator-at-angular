c80a9cb0eb1309ea3b0ef6a8911bb314d4929989
Tokyo <ahmed.tokyo1@gmail.com>
2016-11-10 02:43:43 +0200

    generator nested directive in component basic files and templates creating

--- generators/directive/src/files.js
+++ generators/directive/src/files.js
@@ -6,52 +6,46 @@ var utils = require("../../../utils.js");
 module.exports = function (AngularATGenerator) {
 
     AngularATGenerator.prototype.copyCopmFiles = function copyFiles() {
-      var itemRelPath = this.props.directiveName;
-      var relPathAsArray = itemRelPath.split('/');
+      var relPathAsArray = this.props.directiveName.split('/');
       var directiveName = relPathAsArray[relPathAsArray.length-1];
-
-      var data = {'directiveName': directiveName, 'directiveNameCamel': _.camelCase(directiveName), 'needsPartial': this.props.needsPartial};
       var fullPath;
+      var data = {'directiveName': directiveName, 'directiveNameCamel': _.camelCase(directiveName), 'needsPartial': this.props.needsPartial};
+      // if the directive has no parent, it is shared and belongs to the app
       if(relPathAsArray.length===1) {
         var appRelPath = '/src/app/core/directives';
         fullPath = this.destinationRoot()+appRelPath+'/'+data.directiveName;
-
-        if(this.props.needsPartial){
-        this.fs.copyTpl(
-        this.templatePath('directive.html'),
-        this.destinationPath(fullPath+'/'+data.directiveName+'.html'),
-        data
-      );
-        this.fs.copyTpl(
-        this.templatePath('directive.scss'),
-        this.destinationPath(fullPath+'/'+data.directiveName+'.scss'),
-        data
-      );
-      }
         this.fs.copyTpl(
         this.templatePath('directive.directive.js'),
         this.destinationPath(fullPath+'/'+data.directiveName+'.directive'+'.js'),
         data
-      );
-
-      var coreModulesWriteLine = "require('./directives/"+data.directiveName+"/"+data.directiveName+".directive')(shared);";
-      utils.addToFile("core.module.js",coreModulesWriteLine,utils.DIRECTIVE_MARKER,this.destinationRoot()+"/src/app/core");
-
-    }else{
-      var appRelPath = '/src/app/components';
-      var parentName = relPathAsArray[relPathAsArray.length-2];
-      var parentPath = _.join(relPathAsArray.slice(0, relPathAsArray.length-1), '/');
-      fullPath = this.destinationRoot()+appRelPath+'/'+parentPath+'/directives/'+data.directiveName;
-
+        );
+        var coreModulesWriteLine = "require('./directives/"+data.directiveName+"/"+data.directiveName+".directive')(shared);";
+        utils.addToFile("core.module.js",coreModulesWriteLine,utils.DIRECTIVE_MARKER,this.destinationRoot()+"/src/app/core");
+      }else{
+        // if the directive has a parent component, it belongs to that component
+        var appRelPath = '/src/app/components';
+        var parentName = relPathAsArray[relPathAsArray.length-2];
+        var parentPath = _.join(relPathAsArray.slice(0, relPathAsArray.length-1), '/');
+        fullPath = this.destinationRoot()+appRelPath+'/'+parentPath+'/directives/'+data.directiveName;
+        this.fs.copyTpl(
+        this.templatePath('componentDirective.directive.js'),
+        this.destinationPath(fullPath+'/'+data.directiveName+'.directive'+'.js'),
+        data
+        );
+      }
+      //Write view templates if needed
+      if(this.props.needsPartial){
+        this.fs.copyTpl(
+        this.templatePath('directive.html'),
+        this.destinationPath(fullPath+'/'+data.directiveName+'.html'),
+        data
+        );
       this.fs.copyTpl(
-      this.templatePath('componentDirective.directive.js'),
-      this.destinationPath(fullPath+'/'+data.directiveName+'.directive'+'.js'),
-      data
-    );
-
-    }
-
-
+        this.templatePath('directive.scss'),
+        this.destinationPath(fullPath+'/'+data.directiveName+'.scss'),
+        data
+        );
+      }
 
     };
 };
