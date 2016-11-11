04f6dafb9004e95ee6b9515761aa070d9212f923
Tokyo <ahmed.tokyo1@gmail.com>
2016-11-11 19:26:59 +0200

    seperated store service into its own dir

--- generators/app/templates/_src/_app/_core/core.module.js
+++ generators/app/templates/_src/_app/_core/core.module.js
@@ -4,7 +4,7 @@ const shared = angular.module('core.shared', []);
 
 // Add new directives above
 
-require('./services/store.factory')(shared);
+require('./services/store/store.factory')(shared);
 // Add new services above
 
 export default shared;
