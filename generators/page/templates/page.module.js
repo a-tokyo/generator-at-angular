62f9ee9e3e76cd7975895e2a4f451c31a07d8619
Tokyo <ahmed.tokyo1@gmail.com>
2016-11-12 18:39:58 +0200

    renamed pageModule variable in page.mod.js

--- generators/page/templates/page.module.js
+++ generators/page/templates/page.module.js
@@ -3,10 +3,10 @@
 import * as route from './<%= pageName %>.route';
 import './<%= pageName %>.scss';
 
-const <%= pageName %>Module = angular.module('<%= pageModule %>', [
+const <%= pageModule %>Module = angular.module('<%= pageModule %>', [
   'ui.router'
 ]);
 
 <%= pageName %>Module.config(route);
 
-export default <%= pageName %>Module;
+export default <%= pageModule %>Module;
