1b67101e1f63fbaba9062eec30d0209cc5116b0a
Tokyo <ahmed.tokyo1@gmail.com>
2017-01-19 21:02:11 +0200

    quck edit page controller spec

--- generators/page/templates/page.controller-spec.js
+++ generators/page/templates/page.controller-spec.js
@@ -6,17 +6,17 @@ describe('<%= controllerName %> controller', () => {
 
  beforeEach(() => {
     angular
-      .module('dummyModule',[])
-      .controller('<%= controllerName %>',<%= controllerName %>Controller);    
+      .module('dummyModule', [])
+      .controller('<%= controllerName %>', <%= controllerName %>Controller);
     angular.mock.module('dummyModule');
   });
 
-   it('Should say Hello', angular.mock.inject(($rootScope, $compile,$controller,$log) => {
+   it('Should say Hello', angular.mock.inject(($rootScope, $compile, $controller, $log) => {
     var scope = $rootScope.$new();
     const ctrl = $controller('<%= controllerName %>', {$scope: scope});
-    
+
     expect($log.debug.logs[0]).toEqual(['Hello from <%= controllerName %>Controller!']);
-   
+
   }));
 
 });
