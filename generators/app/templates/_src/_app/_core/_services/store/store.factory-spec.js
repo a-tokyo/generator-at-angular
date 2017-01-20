c62883166055a516a4e44739e424d6572762ba84
Tokyo <ahmed.tokyo1@gmail.com>
2017-01-20 17:23:03 +0200

    add CLIVE's tests as in component to service

---
+++ generators/app/templates/_src/_app/_core/_services/store/store.factory-spec.js
@@ -0,0 +1,15 @@
+import angular from 'angular';
+import 'angular-mocks';
+
+describe('store service', () => {
+
+  beforeEach(() => {
+      const dummyModule = angular.module('dummyModule', []);
+      require('./store.factory')(dummyModule);
+      angular.mock.module('dummyModule');
+  });
+
+  it('should...', angular.mock.inject((store) => {
+    //    expect(store.getData()).toEqual(3); //  Example test
+  }));
+});
