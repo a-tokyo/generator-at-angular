b5faef9b65af30f3927b3d68d56b0fb0fe287df3
Tokyo <ahmed.tokyo1@gmail.com>
2017-01-21 01:52:27 +0200

    _docs enhance _docs.js

--- generators/app/templates/_docs/_docs-assets/_docs.js
+++ generators/app/templates/_docs/_docs-assets/_docs.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
+    //Setting copyright date
     document.getElementById('copyright-year').innerHTML = new Date().getFullYear();
+    // preparing to loop over json file extracting the data and writing it to html
+    var componentCurrIndex;
+    var directiveCurrIndex;
+    var serviceCurrIndex;
+    var pageCurrIndex;
 });
