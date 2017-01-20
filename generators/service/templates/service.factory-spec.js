b6bb1755985d2911c076a0b8dd9cbdffa193a1a1
Clive Makamara <cmosh@live.com>
2017-01-20 12:42:55 +0200

    Improved service factory test template
    - Added an example case to the test

--- generators/service/templates/service.factory-spec.js
+++ generators/service/templates/service.factory-spec.js
@@ -2,6 +2,14 @@ import angular from 'angular';
 import 'angular-mocks';
 
 describe('<%= serviceNameCamel %> service', () => {
-  it('should...', angular.mock.inject(($rootScope, $compile) => {
+
+  beforeEach(() => {
+      const dummyModule = angular.module('dummyModule', []);
+      require('./<%= serviceName %>.factory')(dummyModule);
+      angular.mock.module('dummyModule');
+  });
+
+  it('should...', angular.mock.inject((<%= serviceNameCamel %>) => {
+      //  expect(<%= serviceNameCamel %>.getData()).toEqual(3); //  Example test 
   }));
 });
