d3ffffca896b5be419ced5e80d461da00bc242f9
Tokyo <ahmed.tokyo1@gmail.com>
2017-01-21 02:08:21 +0200

    _docs.js init to read from _docs.json

--- generators/app/templates/_docs/_docs-assets/_docs.js
+++ generators/app/templates/_docs/_docs-assets/_docs.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     //Setting copyright date
     document.getElementById('copyright-year').innerHTML = new Date().getFullYear();
+    // loading docs JSON object
+    var docs;
     // preparing to loop over json file extracting the data and writing it to html
     var componentCurrIndex;
     var directiveCurrIndex;
