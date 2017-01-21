7ced3b146259935d6dbe89fb2bdf3dff0c718588
Tokyo <ahmed.tokyo1@gmail.com>
2017-01-21 14:01:51 +0200

    removed buggy scroll script from _docs

--- generators/app/templates/_docs/_docs-assets/_docs.js
+++ generators/app/templates/_docs/_docs-assets/_docs.js
@@ -26,18 +26,6 @@ var docs =
 $(document).ready(function () {
     //Setting copyright date
     document.getElementById('copyright-year').innerHTML = new Date().getFullYear();
-    $('a[href*="#"]:not([href="#"])').click(function() {
-  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    if (target.length) {
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 1000);
-      return false;
-    }
-  }
-});
 });
 
 
