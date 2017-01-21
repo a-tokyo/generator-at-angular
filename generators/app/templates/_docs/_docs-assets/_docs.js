874ede4c3382bd70233295264817f26dbdf06a44
Tokyo <ahmed.tokyo1@gmail.com>
2017-01-21 10:33:25 +0200

    add var docs to _docs.js

--- generators/app/templates/_docs/_docs-assets/_docs.js
+++ generators/app/templates/_docs/_docs-assets/_docs.js
@@ -1,3 +1,20 @@
+var docs =
+{
+  "components": [
+    {"name": "test-component", "path": "test-component", "description": "", "components": [], "directives": [], "services": []}
+  ],
+  "directives": [
+    {"name": "testDirective", "nameCamel": "testDirective", "path": "test-directive", "description": ""}
+  ],
+  "services": [
+    {"name": "store", "nameCamel": "store", "path": "store", "description": "Store service fetches and holds some application data like arrays for dropdowns and so on."}
+  ],
+  "pages": [
+    {"name": "home", "route": "/", "state": "home", "description": "home page"}
+  ],
+  "info": {"port": 8080}
+};
+
 $(document).ready(function () {
     //Setting copyright date
     document.getElementById('copyright-year').innerHTML = new Date().getFullYear();
@@ -6,4 +23,5 @@ $(document).ready(function () {
     var directiveCurrIndex;
     var serviceCurrIndex;
     var pageCurrIndex;
+    console.log(docs);
 });
