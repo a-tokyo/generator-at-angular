c4715397adc1f77552d8d2e07c4770ef7e39e702
Clive Makamara <cmosh@live.com>
2017-01-21 09:54:13 +0200

    Rewrote the directive test
    - Added a commented example of a test like the service directive
    - Added myself to npm contributors list

--- generators/directive/templates/directive.directive-spec.js
+++ generators/directive/templates/directive.directive-spec.js
@@ -2,6 +2,18 @@ import angular from 'angular';
 import 'angular-mocks';
 
 describe('<%= directiveNameCamel %> directive', () => {
-  it('should...', angular.mock.inject(($rootScope, $compile) => {
+  beforeEach(() => {
+
+      var dummyModule =  angular.module('dummyModule',[]);
+      require('./<%= directiveName %>.directive')(dummyModule);
+      angular.mock.module('dummyModule');
+
+  });
+  it('should...', angular.mock.inject(($rootScope, $compile) => { 
+
+    // const element =  $compile('<<%= directiveName %>></<%= directiveName %>>')($rootScope);
+    // expect(element.html()).toContain('<h1>hello</h1>');
+    // $rootScope.$digest();
+
   }));
 });
