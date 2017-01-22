29c64717432cfc4b3ab1027c39f8faddd7baeca5
Tokyo <ahmed.tokyo1@gmail.com>
2017-01-22 03:44:05 +0200

    remove test services and directives from docs.js

--- generators/app/templates/_docs/docs-assets/docs.js
+++ generators/app/templates/_docs/docs-assets/docs.js
@@ -6,7 +6,6 @@ var docs =
     // Add new components above
   ],
   "directives": [
-    {"name": "testDirective", "nameCamel": "testDirective", "path": "test-directive", "description": "Test directive for Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid."},
     // Add new directives above
   ],
   "services": [
@@ -14,7 +13,6 @@ var docs =
     // Add new services above
   ],
   "pages": [
-    {"name": "home", "route": "/", "state": "home", "description": "home page"},
     // Add new pages above
   ],
   "info": {"appName":"MyATApp", "port": 8080}
