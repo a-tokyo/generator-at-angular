7c7fd2c8ed35a11e0d34b44275bcadd4d9d78b18
Tokyo <ahmed.tokyo1@gmail.com>
2017-01-24 13:36:57 +0200

    in prompts js use let and const

--- generators/app/src/modules.js
+++ generators/app/src/modules.js
@@ -3,7 +3,7 @@ const utils = require("./utils");
 
 module.exports = function (AngularATGenerator) {
 
-  var imports = [];
+  let imports = [];
 
   AngularATGenerator.prototype.defaultImports = function defaultImports() {
     imports.push(this.props.resource);
@@ -20,7 +20,7 @@ module.exports = function (AngularATGenerator) {
    * Compute Angular's module to load and format the dependency list to insert
    */
   AngularATGenerator.prototype.computeModules = function computeModules() {
-    var ngModules = this.props.angularModules.map(function (module) {
+    let ngModules = this.props.angularModules.map(function (module) {
       return module.module;
     });
 
