c507e0af27c02128c0b4b188a0c3aeb9809cd17c
Tokyo <ahmed.tokyo1@gmail.com>
2017-01-24 21:42:13 +0200

    page spec uses let instead of var

--- generators/page/templates/_page.controller-spec.js
+++ generators/page/templates/_page.controller-spec.js
@@ -12,7 +12,7 @@ describe('<%= controllerName %> controller', () => {
   });
 
   it('Should say Hello', angular.mock.inject(($rootScope, $compile, $controller, $log) => {
-    var scope = $rootScope.$new();
+    let scope = $rootScope.$new();
     const ctrl = $controller('<%= controllerName %>', {$scope: scope});
     expect($log.debug.logs[0]).toEqual(['Hello from <%= controllerName %>Controller!']);
   }));
