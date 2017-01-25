464a9e8e9da8a648c76ea8dc7b0ab3a3722857fb
Tokyo <ahmed.tokyo1@gmail.com>
2017-01-25 21:35:39 +0200

    core directive testing complete

--- test/directive-spec.js
+++ test/directive-spec.js
@@ -38,7 +38,7 @@ describe('at-angular:directive no partial', function() {
       fs.copySync(path.join(__dirname, '../generators/app/templates/_src/_app/_core/_core.module.js'), dir + '/src/app/core/core.module.js');
       fs.copySync(path.join(__dirname, '../generators/app/templates/_docs/'), dir + '/docs/');
     })
-    .withPrompts({appName: "testApp", bootstrapSass: false});
+    .withPrompts({needsPartial: false});
   });
 
   it('should create the expected directive files', function(done) {
@@ -47,8 +47,35 @@ describe('at-angular:directive no partial', function() {
       'src/app/core/directives/directive/directive.directive.js'
     ];
     assert.file(expected);
-    // calling done
-    done();
+    const notExpected = [
+      'src/app/core/directives/directive/directive.directive.html',
+      'src/app/core/directives/directive/directive.directive.scss'
+    ];
+    fs.stat('src/app/core/directives/directive/directive.directive.html', function(err, stat){
+      assert(err!=null);
+      // calling done
+      done();
+    });
+  });
+});
+
+describe('at-angular:directive named-directive', function() {
+  beforeEach(function() {
+    return helpers.run(path.join(__dirname, '../generators/directive')).inTmpDir(function(dir) {
+      console.log('running in tmp dir:\n' + dir + '\n')
+      mkdirp('src/app/core/directives');
+      fs.copySync(path.join(__dirname, '../generators/app/templates/_src/_app/_core/_core.module.js'), dir + '/src/app/core/core.module.js');
+      fs.copySync(path.join(__dirname, '../generators/app/templates/_docs/'), dir + '/docs/');
+    })
+    .withArguments(['named-directive']);
   });
 
+  it('should create the expected directive files', function(done) {
+    const expected = [
+      'src/app/core/directives/named-directive/named-directive.directive-spec.js',
+      'src/app/core/directives/named-directive/named-directive.directive.js'
+    ];
+    assert.file(expected);
+    done();
+  });
 });
