5574587b521c34b22d151534ad2eba09dbf7bce7
Tokyo <ahmed.tokyo1@gmail.com>
2017-01-25 23:45:31 +0200

    nested directive tests (may need to enhance)

--- test/directive-spec.js
+++ test/directive-spec.js
@@ -78,3 +78,25 @@ describe('at-angular:directive named-directive', function() {
     done();
   });
 });
+
+describe('at-angular:directive component/directive', function() {
+  beforeEach(function() {
+    return helpers.run(path.join(__dirname, '../generators/directive')).inTmpDir(function(dir) {
+      console.log('running in tmp dir:\n' + dir + '\n')
+      fs.mkdirp('src/app/components/component');
+      fs.copySync(path.join(__dirname, '../generators/app/templates/_src/_app/_index.components.js'), dir + '/src/app/index.components.js');
+      fs.copySync(path.join(__dirname, '../generators/component/templates/_component.module.js'), dir + '/src/app/components/component/component.module.js');
+      fs.copySync(path.join(__dirname, '../generators/app/templates/_docs/'), dir + '/docs/');
+    })
+    .withArguments(['component/named-directive']);
+  });
+
+  it('should create the expected directive files', function(done) {
+    const expected = [
+      'src/app/components/component/directives/named-directive/named-directive.directive-spec.js',
+      'src/app/components/component/directives/named-directive/named-directive.directive.js'
+    ];
+    assert.file(expected);
+    done();
+  });
+});
