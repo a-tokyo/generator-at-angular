3fd2713a41af9044e3c8bfaac6dade506cea9848
Tokyo <ahmed.tokyo1@gmail.com>
2017-01-25 20:10:46 +0200

    add page testing

---
+++ test/page-spec.js
@@ -0,0 +1,32 @@
+'use strict';
+const path = require('path');
+const helpers = require('yeoman-test');
+const assert = require('assert');
+const fs = require('fs-extra');
+const mkdirp = require('mkdirp');
+
+describe('at-angular:page', function() {
+  beforeEach(function() {
+    return helpers.run(path.join(__dirname, '../generators/page')).inTmpDir(function(dir) {
+      console.log('running in tmp dir:\n' + dir + '\n')
+      mkdirp('src/app/pages');
+      fs.copySync(path.join(__dirname, '../generators/app/templates/_src/_app/_index.module.js'), dir + '/src/app/index.module.js');
+      fs.copySync(path.join(__dirname, '../generators/app/templates/_docs/'), dir + '/docs/');
+    })
+  });
+
+  it('should create the expected page files', function(done) {
+    const expected = [
+      'src/app/pages/page/page.controller-spec.js',
+      'src/app/pages/page/page.controller.js',
+      'src/app/pages/page/page.html',
+      'src/app/pages/page/page.module.js',
+      'src/app/pages/page/page.route.js',
+      'src/app/pages/page/page.scss'
+    ];
+    assert.file(expected);
+    // calling done
+    done();
+  });
+
+});
