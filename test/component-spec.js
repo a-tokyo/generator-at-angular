b0c5dd8b7dd9ef99e628ced76b0b09906c2c9d73
Tokyo <ahmed.tokyo1@gmail.com>
2017-01-25 23:47:44 +0200

    nested component tests (may need to enhance)

--- test/component-spec.js
+++ test/component-spec.js
@@ -28,3 +28,25 @@ describe('at-angular:component', function() {
   });
 
 });
+
+describe('at-angular:component component/component', function() {
+  beforeEach(function() {
+    return helpers.run(path.join(__dirname, '../generators/component')).inTmpDir(function(dir) {
+      console.log('running in tmp dir:\n' + dir + '\n')
+      fs.mkdirp('src/app/components/component');
+      fs.copySync(path.join(__dirname, '../generators/app/templates/_src/_app/_index.components.js'), dir + '/src/app/index.components.js');
+      fs.copySync(path.join(__dirname, '../generators/component/templates/_component.module.js'), dir + '/src/app/components/component/component.module.js');
+      fs.copySync(path.join(__dirname, '../generators/app/templates/_docs/'), dir + '/docs/');
+    })
+    .withArguments(['component/named-component']);
+  });
+
+  it('should create the expected component files', function(done) {
+    const expected = [
+      'src/app/components/component/components/named-component/named-component.component-spec.js',
+      'src/app/components/component/components/named-component/named-component.component.js'
+    ];
+    assert.file(expected);
+    done();
+  });
+});
