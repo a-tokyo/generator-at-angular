8c0f6353ef8507415825e9c1a5ed880f4dc9cc85
Tokyo <ahmed.tokyo1@gmail.com>
2017-01-25 04:21:32 +0200

    add mocha and test folder

---
+++ test/test.js
@@ -0,0 +1,12 @@
+'use strict';
+const path = require('path');
+const helpers = require('yeoman-test');
+const assert = require('assert');
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+});
