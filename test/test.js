b9bbdbbf2a3842a6d37db2e252edb25d4c2936a1
Tokyo <ahmed.tokyo1@gmail.com>
2017-01-25 16:26:49 +0200

    add basic files existing text for all but src

--- test/test.js
+++ test/test.js
@@ -2,11 +2,37 @@
 const path = require('path');
 const helpers = require('yeoman-test');
 const assert = require('assert');
+const fs = require('fs');
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
+describe('at-angular generator', function () {
+  beforeEach(function () {
+  // The object returned act like a promise, so return it to wait until the process is done
+  return helpers.run(path.join(__dirname, '../generators/app'))
+    .withPrompts({ appName: "testApp",bootstrapSass: false }); // Mock the prompt answers
+  });
+
+  it('creates expected files', function (done) {
+    const expected = [
+      // add files you expect to exist here.
+      'config/webpack/global.js',
+      'config/webpack/environments/development.js',
+      'config/webpack/environments/production.js',
+      'docs/docs.html',
+      'docs/docs-assets/docs-app.js',
+      'docs/docs-assets/docs.css',
+      'docs/docs-assets/docs.js',
+      '.babelrc',
+      '.editorconfig',
+      '.eslintrc.json',
+      '.yo-rc.json',
+      'karma.conf.js',
+      'package.json',
+      'README.md',
+      'test-context.js',
+      'webpack.config.js'
+    ];
+    assert.file(expected);
+    // calling done
+    done();
   });
 });
