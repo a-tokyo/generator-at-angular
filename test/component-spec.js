84dc1c37543604ce8abfed079ee0178e3ded96c1
Tokyo <ahmed.tokyo1@gmail.com>
2017-01-26 02:38:09 +0200

    auto remove testDir afterEach test

--- test/component-spec.js
+++ test/component-spec.js
@@ -4,15 +4,23 @@ const helpers = require('yeoman-test');
 const assert = require('assert');
 const fs = require('fs-extra');
 const testUtils = require('./test-utils');
+let testDir = null;
 
 describe('at-angular:component', function() {
   beforeEach(function() {
     return helpers.run(path.join(__dirname, '../generators/component')).inTmpDir(function(dir) {
       testUtils.logIf('running in tmp dir:\n' + dir + '\n', testUtils.debugMode)
+      testDir = dir;
       fs.mkdirp('src/app/components');
       fs.copySync(path.join(__dirname, '../generators/app/templates/_src/_app/_index.components.js'), dir + '/src/app/index.components.js');
       fs.copySync(path.join(__dirname, '../generators/app/templates/_docs/'), dir + '/docs/');
     })
+  });
+
+  afterEach(function(){
+    if(testDir != null){
+      testUtils.deleteDirRecursive(testDir);
+    }
   });
 
   it('should create the expected component files', function(done) {
@@ -33,12 +41,19 @@ describe('at-angular:component component/component', function() {
   beforeEach(function() {
     return helpers.run(path.join(__dirname, '../generators/component')).inTmpDir(function(dir) {
       testUtils.logIf('running in tmp dir:\n' + dir + '\n', testUtils.debugMode)
+      testDir = dir;
       fs.mkdirp('src/app/components/component');
       fs.copySync(path.join(__dirname, '../generators/app/templates/_src/_app/_index.components.js'), dir + '/src/app/index.components.js');
       fs.copySync(path.join(__dirname, '../generators/component/templates/_component.module.js'), dir + '/src/app/components/component/component.module.js');
       fs.copySync(path.join(__dirname, '../generators/app/templates/_docs/'), dir + '/docs/');
     })
     .withArguments(['component/named-component']);
+  });
+
+  afterEach(function(){
+    if(testDir != null){
+      testUtils.deleteDirRecursive(testDir);
+    }
   });
 
   it('should create the expected component files', function(done) {
--- test/page-spec.js
+++ test/page-spec.js
@@ -4,15 +4,23 @@ const helpers = require('yeoman-test');
 const assert = require('assert');
 const fs = require('fs-extra');
 const testUtils = require('./test-utils');
+let testDir = null;
 
 describe('at-angular:page', function() {
   beforeEach(function() {
     return helpers.run(path.join(__dirname, '../generators/page')).inTmpDir(function(dir) {
       testUtils.logIf('running in tmp dir:\n' + dir + '\n', testUtils.debugMode)
+      testDir = dir;
       fs.mkdirp('src/app/pages');
       fs.copySync(path.join(__dirname, '../generators/app/templates/_src/_app/_index.module.js'), dir + '/src/app/index.module.js');
       fs.copySync(path.join(__dirname, '../generators/app/templates/_docs/'), dir + '/docs/');
     })
+  });
+
+  afterEach(function(){
+    if(testDir != null){
+      testUtils.deleteDirRecursive(testDir);
+    }
   });
 
   it('should create the expected page files', function(done) {
