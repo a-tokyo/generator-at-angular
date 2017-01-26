012d03cc39ad15f61befbdc0c0e32bdebeefd591
Tokyo <ahmed.tokyo1@gmail.com>
2017-01-26 22:49:36 +0200

    add dreidev option

--- generators/app/src/prompts.js
+++ generators/app/src/prompts.js
@@ -4,47 +4,53 @@ const chalk = require('chalk');
 const prompts = require('../prompts.json');
 
 function logChoice(prompt, prop) {
-    let choice = _.find(prompt.choices, {value: prop});
-    this.log('\t*', choice.name);
+  let choice = _.find(prompt.choices, {value: prop});
+  this.log('\t*', choice.name);
 }
 
-module.exports = function (AngularATGenerator) {
+function getDefaultProps() {
+  let defaults = {};
+  // get defaults from prompts.json
+  _.forIn(prompts, function(message) {
+    defaults[message.name] = message.name;
+    defaults[message.name] = message.default;
+  });
+  return defaults;
+}
 
-     /**
+function getDreidevProps() {
+  let dreidevProps = getDefaultProps();
+  dreidevProps.bootstrapSass = false;
+  dreidevProps.appName = 'dreidevATApp';
+  dreidevProps.angularModules.push({key: 'at-flex-grid', module: null, package: 'at-flex-grid'});
+  return dreidevProps;
+}
+
+module.exports = function(AngularATGenerator) {
+
+  /**
      * Ask all questions from prompts.json
      * Add conditional tests on those depending on first responses
      * Complete responses with null answers for questions not asked
      */
-    AngularATGenerator.prototype.askQuestions = function askQuestions() {
-        if (this.options.default) {
-
-            let defaults = {};
-            // get defaults from prompts.json
-            _.forIn(prompts, function(message) {  
-                defaults[message.name] = message.name;
-                defaults[message.name]= message.default;
-            });
-            // calling default options          
-            this.props = _.merge(this.props, defaults);
-            return;
-
-        }else if(this.skipConfig) {
-
-            return;
-
-        }else {
-
-            let done = this.async();
-            // calling prompts async
-            this.prompt(prompts, function (props) {
-
-                    this.props = _.merge(this.props, props);
-                    // calling done to continue run loop
-                    done();
-                }.bind(this));
-
-            }
-
-    };
-
+  AngularATGenerator.prototype.askQuestions = function askQuestions() {
+    if (this.skipConfig) {
+      return;
+    } else if (this.options.default) {
+      // calling default options
+      this.props = _.merge(this.props, getDefaultProps());
+      return;
+    } else if (this.options.dreidev) {
+      this.props = _.merge(this.props, getDreidevProps());
+      return;
+    } else {
+      let done = this.async();
+      // calling prompts async
+      this.prompt(prompts, function(props) {
+        this.props = _.merge(this.props, props);
+        // calling done to continue run loop
+        done();
+      }.bind(this));
+    }
+  };
 };
