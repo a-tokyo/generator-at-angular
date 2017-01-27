03b837bffd4d89c2c1673ba3bee0178e3c124766
Tokyo <ahmed.tokyo1@gmail.com>
2017-01-27 14:42:44 +0200

    refactor app test

--- test/app-spec.js
+++ test/app-spec.js
@@ -2,230 +2,68 @@
 const path = require('path');
 const helpers = require('yeoman-test');
 const assert = require('assert');
-// const fs = require('fs');
+
+// const expected files
+const expectedConfigFiles = ['config/webpack/global.js', 'config/webpack/environments/development.js', 'config/webpack/environments/production.js', 'webpack.config.js'];
+const expectedDocsFiles = ['docs/docs.html', 'docs/docs-assets/docs-app.js', 'docs/docs-assets/docs.css', 'docs/docs-assets/docs.js'];
+const expectedGeneralFiles = [
+  '.babelrc',
+  '.editorconfig',
+  '.eslintrc.json',
+  '.yo-rc.json',
+  'karma.conf.js',
+  'package.json',
+  'README.md',
+  'test-context.js'
+];
+const expectedAngularAppFiles = [
+  'src/assets/styles/sass/fonts.scss',
+  'src/assets/styles/sass/index.scss',
+  'src/assets/styles/sass/main.scss',
+  'src/favicon.ico',
+  'src/tpl-index.ejs',
+  'src/app/components',
+  'src/app/core/core.module.js',
+  'src/app/index.bootstrap.js',
+  'src/app/index.components.js',
+  'src/app/index.config.js',
+  'src/app/index.module.js',
+  'src/app/index.routes.js',
+  'src/app/index.run.js',
+  'src/app/index.vendor.js',
+  'src/app/pages'
+];
+
+const expectedFiles = expectedConfigFiles.concat(expectedConfigFiles).concat(expectedDocsFiles).concat(expectedGeneralFiles).concat(expectedAngularAppFiles);
 
 describe('at-angular', function() {
   beforeEach(function() {
-    // The object returned act like a promise, so return it to wait until the process is done
-    return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({appName: "testApp"}); // Mock some prompt answers, and depend on defaults for others
+    return helpers.run(path.join(__dirname, '../generators/app')).withPrompts({appName: "testApp"}); // Mock some prompt answers, and depend on defaults for others
   });
-
-  it('should create the expected webpack config files', function(done) {
-    const expected = [
-      // add files you expect to exist here.
-      'config/webpack/global.js',
-      'config/webpack/environments/development.js',
-      'config/webpack/environments/production.js',
-      'webpack.config.js'
-    ];
-    assert.file(expected);
-    // calling done
-    done();
-  });
-
-  it('should create the expected documentation files', function(done) {
-    const expected = [
-      // add files you expect to exist here.
-      'docs/docs.html',
-      'docs/docs-assets/docs-app.js',
-      'docs/docs-assets/docs.css',
-      'docs/docs-assets/docs.js'
-    ];
-    assert.file(expected);
-    // calling done
-    done();
-  });
-
-  it('should create the expected general files', function(done) {
-    const expected = [
-      // add files you expect to exist here.
-      '.babelrc',
-      '.editorconfig',
-      '.eslintrc.json',
-      '.yo-rc.json',
-      'karma.conf.js',
-      'package.json',
-      'README.md',
-      'test-context.js'
-    ];
-    assert.file(expected);
-    // calling done
-    done();
-  });
-
   it('should create the expected angular application files', function(done) {
-    const expected = [
-      // add files you expect to exist here.
-      'src/assets/styles/sass/fonts.scss',
-      'src/assets/styles/sass/index.scss',
-      'src/assets/styles/sass/main.scss',
-      'src/favicon.ico',
-      'src/tpl-index.ejs',
-      'src/app/components',
-      'src/app/core/core.module.js',
-      'src/app/index.bootstrap.js',
-      'src/app/index.components.js',
-      'src/app/index.config.js',
-      'src/app/index.module.js',
-      'src/app/index.routes.js',
-      'src/app/index.run.js',
-      'src/app/index.vendor.js',
-      'src/app/pages'
-    ];
-    assert.file(expected);
-    // calling done
+    assert.file(expectedAngularAppFiles);
     done();
   });
 });
 
-
 describe('at-angular --dreidev', function() {
   beforeEach(function() {
-    // The object returned act like a promise, so return it to wait until the process is done
-    return helpers.run(path.join(__dirname, '../generators/app'))
-      .withOptions({dreidev: true}); // Mock some prompt answers, and depend on defaults for others
-  });
-
-  it('should create the expected webpack config files', function(done) {
-    const expected = [
-      // add files you expect to exist here.
-      'config/webpack/global.js',
-      'config/webpack/environments/development.js',
-      'config/webpack/environments/production.js',
-      'webpack.config.js'
-    ];
-    assert.file(expected);
-    // calling done
-    done();
-  });
-
-  it('should create the expected documentation files', function(done) {
-    const expected = [
-      // add files you expect to exist here.
-      'docs/docs.html',
-      'docs/docs-assets/docs-app.js',
-      'docs/docs-assets/docs.css',
-      'docs/docs-assets/docs.js'
-    ];
-    assert.file(expected);
-    // calling done
-    done();
-  });
-
-  it('should create the expected general files', function(done) {
-    const expected = [
-      // add files you expect to exist here.
-      '.babelrc',
-      '.editorconfig',
-      '.eslintrc.json',
-      '.yo-rc.json',
-      'karma.conf.js',
-      'package.json',
-      'README.md',
-      'test-context.js'
-    ];
-    assert.file(expected);
-    // calling done
-    done();
+    return helpers.run(path.join(__dirname, '../generators/app')).withOptions({dreidev: true}); // Mock some prompt answers, and depend on defaults for others
   });
 
   it('should create the expected angular application files', function(done) {
-    const expected = [
-      // add files you expect to exist here.
-      'src/assets/styles/sass/fonts.scss',
-      'src/assets/styles/sass/index.scss',
-      'src/assets/styles/sass/main.scss',
-      'src/favicon.ico',
-      'src/tpl-index.ejs',
-      'src/app/components',
-      'src/app/core/core.module.js',
-      'src/app/index.bootstrap.js',
-      'src/app/index.components.js',
-      'src/app/index.config.js',
-      'src/app/index.module.js',
-      'src/app/index.routes.js',
-      'src/app/index.run.js',
-      'src/app/index.vendor.js',
-      'src/app/pages'
-    ];
-    assert.file(expected);
-    // calling done
+    assert.file(expectedAngularAppFiles);
     done();
   });
 });
 
 describe('at-angular --default', function() {
   beforeEach(function() {
-    // The object returned act like a promise, so return it to wait until the process is done
-    return helpers.run(path.join(__dirname, '../generators/app'))
-      .withOptions({default: true}); // Mock some prompt answers, and depend on defaults for others
-  });
-
-  it('should create the expected webpack config files', function(done) {
-    const expected = [
-      // add files you expect to exist here.
-      'config/webpack/global.js',
-      'config/webpack/environments/development.js',
-      'config/webpack/environments/production.js',
-      'webpack.config.js'
-    ];
-    assert.file(expected);
-    // calling done
-    done();
-  });
-
-  it('should create the expected documentation files', function(done) {
-    const expected = [
-      // add files you expect to exist here.
-      'docs/docs.html',
-      'docs/docs-assets/docs-app.js',
-      'docs/docs-assets/docs.css',
-      'docs/docs-assets/docs.js'
-    ];
-    assert.file(expected);
-    // calling done
-    done();
-  });
-
-  it('should create the expected general files', function(done) {
-    const expected = [
-      // add files you expect to exist here.
-      '.babelrc',
-      '.editorconfig',
-      '.eslintrc.json',
-      '.yo-rc.json',
-      'karma.conf.js',
-      'package.json',
-      'README.md',
-      'test-context.js'
-    ];
-    assert.file(expected);
-    // calling done
-    done();
+    return helpers.run(path.join(__dirname, '../generators/app')).withOptions({default: true}); // Mock some prompt answers, and depend on defaults for others
   });
 
   it('should create the expected angular application files', function(done) {
-    const expected = [
-      // add files you expect to exist here.
-      'src/assets/styles/sass/fonts.scss',
-      'src/assets/styles/sass/index.scss',
-      'src/assets/styles/sass/main.scss',
-      'src/favicon.ico',
-      'src/tpl-index.ejs',
-      'src/app/components',
-      'src/app/core/core.module.js',
-      'src/app/index.bootstrap.js',
-      'src/app/index.components.js',
-      'src/app/index.config.js',
-      'src/app/index.module.js',
-      'src/app/index.routes.js',
-      'src/app/index.run.js',
-      'src/app/index.vendor.js',
-      'src/app/pages'
-    ];
-    assert.file(expected);
-    // calling done
+    assert.file(expectedAngularAppFiles);
     done();
   });
 });
