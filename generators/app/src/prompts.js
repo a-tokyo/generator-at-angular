ebec4c1becd79dc8b6bd7d05b7dc54ecd0756617
Tokyo <ahmed.tokyo1@gmail.com>
2017-01-28 06:13:00 +0200

    finalize basic dreidev option

--- generators/app/src/prompts.js
+++ generators/app/src/prompts.js
@@ -42,7 +42,7 @@ module.exports = function(AngularATGenerator) {
       return;
     } else if (this.options.dreidev) {
       this.props = _.merge(this.props, getDreidevProps());
-      this.props.extraDeps = [{dependency: "at-flex-grid", package: "at-flex-grid"}];
+      this.props.extraDeps = [{dependency: "at-flex-grid"}];
       return;
     } else {
       let done = this.async();
