e9506bc1aaddb34876ec0774be257011641546da
Tokyo <ahmed.tokyo1@gmail.com>
2017-01-29 03:32:06 +0200

    new docs angular app

--- generators/app/templates/_docs/docs-assets/docs.js
+++ generators/app/templates/_docs/docs-assets/docs.js
@@ -1,16 +1,18 @@
 // generator-at-angular-docs angular application
 var docsApp = angular.module('generator-at-angular-docs', []);
 // Docs main controller
-docsApp.controller('docsCtrl', function ($scope){
-  // attaching appName to scope
-  $scope.appName = docs.info.appName;
-  // attaching docs to scope
-  $scope.components = docs.components;
-  $scope.directives = docs.directives;
-  $scope.services = docs.services;
-  $scope.pages = docs.pages;
-  // Base href for app routes
-  $scope.apphost = 'http://localhost:'+docs.info.port;
+docsApp.controller('docsCtrl', function ($scope, $http){
+  return $http({'method': "GET", 'url': 'docs-assets/docs.json', cache: false}).then(function(res){
+    // attaching appName to scope
+    $scope.appName = res.data.info.appName;
+    // attaching docs to scope
+    $scope.components = res.data.components;
+    $scope.directives = res.data.directives;
+    $scope.services = res.data.services;
+    $scope.pages = res.data.pages;
+    // Base href for app routes
+    $scope.apphost = 'http://localhost:'+res.data.info.port;
+  });
   // attaching currentYear to scope
   $scope.currentYear = new Date().getFullYear();
   /*
