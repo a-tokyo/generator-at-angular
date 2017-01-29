5a45b146a7b7c211c4facc915dd052df5306de6a
Tokyo <ahmed.tokyo1@gmail.com>
2017-01-29 03:13:18 +0200

    new docs app structure]

--- generators/app/templates/_docs/docs-assets/docs.js
+++ generators/app/templates/_docs/docs-assets/docs.js
@@ -1,19 +1,24 @@
-// Docs in JSON format
-var docs =
-{
-  "components": [
-    // Add new components above
-  ],
-  "directives": [
-    // Add new directives above
-  ],
-  "services": [
-    {"name": "store", "nameCamel": "store", "path": "store", "description": "Store service fetches and holds some application data like arrays for dropdowns and so on."},
-    // Add new services above
-  ],
-  "pages": [
-    // Add new pages above
-  ],
-  "info": {"appName":"<%= props.appName %>", "port": 8080}
-  // Add application info above
-};
+// generator-at-angular-docs angular application
+var docsApp = angular.module('generator-at-angular-docs', []);
+// Docs main controller
+docsApp.controller('docsCtrl', function ($scope){
+  // attaching appName to scope
+  $scope.appName = docs.info.appName;
+  // attaching docs to scope
+  $scope.components = docs.components;
+  $scope.directives = docs.directives;
+  $scope.services = docs.services;
+  $scope.pages = docs.pages;
+  // Base href for app routes
+  $scope.apphost = 'http://localhost:'+docs.info.port;
+  // attaching currentYear to scope
+  $scope.currentYear = new Date().getFullYear();
+  /*
+   * replaceSlashWith2Underscores
+   * takes a string as an input
+   * replaces every '/' with '__'
+   */
+  $scope.replaceSlashWith2Underscores = function(string){
+    return string.replace(new RegExp('/', 'g'), '__');
+  };
+});
