e68e20ad8a1482ae193ef4fb026e0bcd9291a8dc
Tokyo <ahmed.tokyo1@gmail.com>
2017-01-30 05:24:17 +0200

    basic component remover not nested

--- generators/remove/src/files.js
+++ generators/remove/src/files.js
@@ -7,9 +7,21 @@ const jsonQuery = require('json-query');
 module.exports = function(AngularATGenerator) {
 
     AngularATGenerator.prototype.copyCopmFiles = function copyFiles() {
+      const pathAsArray = this.props.split('/');
       switch (this.props.type) {
         case 'component':
-
+          if (pathAsArray.length === 1){
+            let componentName = pathAsArray[pathAsArray.length - 1];
+            try {
+              const indexModulesRemoveLine = "require('./components/" + componentName + "/" + componentName + ".module').name,";
+              utils.removeFromFile('index.components.js', indexModulesRemoveLine, this.destinationRoot() + '/src/app')
+              utils.deleteDirRecursive(this.destinationRoot() + '/src/app/components/' + componentName);
+            } catch (err) {
+              console.log(err);
+            }
+          }else{
+            // component is nested
+          }
           break;
         case 'directive':
 
