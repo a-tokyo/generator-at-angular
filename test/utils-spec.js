c29bf4f4ea9cc82ce7fce8733698dbf8511f73ff
Tokyo <ahmed.tokyo1@gmail.com>
2017-02-01 01:58:43 +0200

    more tests

--- test/utils-spec.js
+++ test/utils-spec.js
@@ -1,6 +1,7 @@
 const genUtils = require('../generators/utils');
 const fs = require('fs-extra');
 const path = require('path');
+const assert = require('assert');
 const should = require('should');
 const testUtils = require('./test-utils');
 
@@ -18,6 +19,17 @@ describe('Utils', function() {
       var dirPath = path.join(__dirname, 'testDir');
       genUtils.deleteDirRecursive(dirPath);
       fs.existsSync(dirPath).should.not.throw();
+    });
+  });
+
+  describe('removeLineFromFile()', function() {
+    it('should not throw when the line is not found', function() {
+      try{
+        genUtils.removeLineFromFile('comp.component.js','BlaBlaBlaLineNoExistStarWarsGameOfThrones', path.join(__dirname, './test-scenarios/test-remove/src/app/components/comp'));
+      }
+      catch(err){
+        assert(err!==null);
+      }
     });
   });
 });
