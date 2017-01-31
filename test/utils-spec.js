ab3e3887a81ae57f79cf4a2e6fc886ccadbd7dfd
Tokyo <ahmed.tokyo1@gmail.com>
2017-01-31 12:16:45 +0200

    add test for utils dirRemove

--- test/utils-spec.js
+++ test/utils-spec.js
@@ -1,10 +1,28 @@
-const testUtils = require('../generators/utils');
+const genUtils = require('../generators/utils');
+const fs = require('fs-extra');
+const path = require('path');
+const should = require('should');
 
 
 describe('Utils', function() {
   describe('stripPackageName()', function() {
     it('should return the appropriate package name to import', function() {
-      testUtils.stripPackageName('at-flex-grid@0.1.0').should.equal('at-flex-grid');
+      genUtils.stripPackageName('at-flex-grid@0.1.0').should.equal('at-flex-grid');
+    });
+  });
+
+  describe('deleteDirRecursive()', function() {
+    it('should delete the appropriate directory', function() {
+      var dirPath = path.join(__dirname, 'testDir');
+      fs.mkdirp(dirPath);
+      genUtils.deleteDirRecursive(dirPath);
+      fs.existsSync(dirPath).should.equal(false);
+    });
+
+    it('should not throw when the dir is not found', function() {
+      var dirPath = path.join(__dirname, 'testDir');
+      genUtils.deleteDirRecursive(dirPath);
+      fs.existsSync(dirPath).should.not.throw();
     });
   });
 });
