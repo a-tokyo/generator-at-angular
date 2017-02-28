b7440b88c35501c8d5192b1bf4eaebd26ed572be
Tokyo <ahmed.tokyo1@gmail.com>
2017-02-28 03:54:02 +0200

    component use template strings

--- generators/component/src/files.js
+++ generators/component/src/files.js
@@ -48,8 +48,8 @@ module.exports = function(AngularATGenerator) {
     //if the component has no parent
     if (pathAsArray.length === 1) {
       try {
-        const indexModulesWriteLine = "require('./components/" + data.componentName + "/" + data.componentName + ".module').name,";
-        utils.addToFile('index.components.js', indexModulesWriteLine, utils.COMPONENT_MARKER, this.destinationRoot() + '/src/app');
+        const indexModulesWriteLine = `require('./components/${data.componentName}/${data.componentName}.module').name,`;
+        utils.addToFile('index.components.js', indexModulesWriteLine, utils.COMPONENT_MARKER, `${this.destinationRoot()}/src/app`);
       } catch (err) {
         this.log('Could not generate this item due to missing file structure.');
         return;
@@ -62,15 +62,18 @@ module.exports = function(AngularATGenerator) {
         ? '/components/'
         : '/';
       parentPath = _.join(pathAsArray.slice(0, pathAsArray.length - 1), joinString);
-      fullPath = parentPath + '/components/' + data.componentName;
+      fullPath = `${parentPath}/components/${data.componentName}`;
       // importing files to parent component
       try {
         //module
-        const moduleImport = "import * as " + data.componentModule + " from './components/" + componentName + '/' + componentName + ".module';";
-        utils.addToFile(parentName + '.module.js', moduleImport, utils.IMPORT_MODULE_MARKER, this.destinationRoot() + '/src/app/components/' + parentPath);
+        // const moduleImport = "import * as " + data.componentModule + " from './components/" + componentName + '/' + componentName + ".module';";
+        const moduleImport = `import * as ${data.componentModule} from './components/${componentName}/${componentName}.module';`;
+        // utils.addToFile(parentName + '.module.js', moduleImport, utils.IMPORT_MODULE_MARKER, this.destinationRoot() + '/src/app/components/' + parentPath);
+        utils.addToFile(`${parentName}.module.js`, moduleImport, utils.IMPORT_MODULE_MARKER, `${this.destinationRoot()}/src/app/components/${parentPath}`);
         //dependency
-        const dependencyImport = "'" + data.componentModule + "',";
-        utils.addToFile(parentName + '.module.js', dependencyImport, utils.IMPORT_DEPENDENCY_MARKER, this.destinationRoot() + '/src/app/components/' + parentPath);
+        // const dependencyImport = "'" + data.componentModule + "',";
+        const dependencyImport = `'${data.componentModule}',`;
+        utils.addToFile(`${parentName}.module.js`, dependencyImport, utils.IMPORT_DEPENDENCY_MARKER, `${this.destinationRoot()}/src/app/components/${parentPath}`);
       } catch (err) {
         this.log('Parent component files not found.');
         return;
@@ -78,14 +81,15 @@ module.exports = function(AngularATGenerator) {
     }
 
     // checking if the module exists, if so it is a duplicate
-    isDuplicate = utils.existsSync(this.destinationPath(this.destinationRoot() + '/src/app/components/' + fullPath + '/' + data.componentName + '.module.js'));
+    // isDuplicate = utils.existsSync(this.destinationPath(this.destinationRoot() + '/src/app/components/' + fullPath + '/' + data.componentName + '.module.js'));
+    isDuplicate = utils.existsSync(this.destinationPath(`${this.destinationRoot()}/src/app/components/${fullPath}/${data.componentName}.module.js`));
 
     // copy template files, no need for try and catch since file structure already exists from above
-    this.fs.copyTpl(this.templatePath('_component.html'), this.destinationPath(this.destinationRoot() + '/src/app/components/' + fullPath + '/' + data.componentName + '.component.html'), data);
-    this.fs.copyTpl(this.templatePath('_component.scss'), this.destinationPath(this.destinationRoot() + '/src/app/components/' + fullPath + '/' + data.componentName + '.component.scss'), data);
-    this.fs.copyTpl(this.templatePath('_component.module.js'), this.destinationPath(this.destinationRoot() + '/src/app/components/' + fullPath + '/' + data.componentName + '.module.js'), data);
-    this.fs.copyTpl(this.templatePath('_component.component.js'), this.destinationPath(this.destinationRoot() + '/src/app/components/' + fullPath + '/' + data.componentName + '.component' + '.js'), data);
-    this.fs.copyTpl(this.templatePath('_component.component-spec.js'), this.destinationPath(this.destinationRoot() + '/src/app/components/' + fullPath + '/' + data.componentName + '.component-spec' + '.js'), data);
+    this.fs.copyTpl(this.templatePath('_component.html'), this.destinationPath(`${this.destinationRoot()}/src/app/components/${fullPath}/${data.componentName}.component.html`), data);
+    this.fs.copyTpl(this.templatePath('_component.scss'), this.destinationPath(`${this.destinationRoot()}/src/app/components/${fullPath}/${data.componentName}.component.scss`), data);
+    this.fs.copyTpl(this.templatePath('_component.module.js'), this.destinationPath(`${this.destinationRoot()}/src/app/components/${fullPath}/${data.componentName}.module.js`), data);
+    this.fs.copyTpl(this.templatePath('_component.component.js'), this.destinationPath(`${this.destinationRoot()}/src/app/components/${fullPath}/${data.componentName}.component.js`), data);
+    this.fs.copyTpl(this.templatePath('_component.component-spec.js'), this.destinationPath(`${this.destinationRoot()}/src/app/components/${fullPath}/${data.componentName}.component-spec.js`), data);
 
     // Documenting the creation of the component
     if (!isDuplicate) {
@@ -97,23 +101,23 @@ module.exports = function(AngularATGenerator) {
         }
         const descriptionForDocs = (this.props.description && this.props.description.length > 0)
           ? this.props.description
-          : data.componentName + " component";
+          : `${data.componentName} component`;
         const componentDocJSON = {
-          "name": data.componentName,
-          "path": this.props.componentName,
-          "components": [],
-          "directives": [],
-          "services": [],
-          "description": descriptionForDocs
+          'name': data.componentName,
+          'path': this.props.componentName,
+          'components': [],
+          'directives': [],
+          'services': [],
+          'description': descriptionForDocs
         };
         docsJSON.components.push(componentDocJSON);
         if (pathAsArray.length !== 1) {
           // Foreign Key String for component is injected into the parent component
           const componentDocForeignKeyJSON = {
-            "path": this.props.componentName,
-            "name": data.componentName
+            'path': this.props.componentName,
+            'name': data.componentName
           };
-          docsJSON.components[jsonQuery('components[path=' + pathAsArray.slice(0, -1).join('/') + ']', {data: docsJSON}).key].components.push(componentDocForeignKeyJSON);
+          docsJSON.components[jsonQuery(`components[path=${pathAsArray.slice(0, -1).join('/')}]`, {data: docsJSON}).key].components.push(componentDocForeignKeyJSON);
         }
         jsonfile.writeFile(file, docsJSON, {spaces: 2}, function(err) {}.bind(this));
       }.bind(this));
